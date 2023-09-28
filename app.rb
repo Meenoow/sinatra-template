8bdac04cdc93acc4441cddb5bc28ec76a1784d4e
Meenoow <100647073+Meenoow@users.noreply.github.com>
2023-09-28 17:46:59 +0000

    Adding random images to the main page everytime it reloads

--- app.rb
+++ app.rb
@@ -1,9 +1,30 @@
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/activerecord"
+require "http"
+require 'nokogiri'
+require 'open-uri'
+
+IMAGE_SOURCE_URL = [
+  'https://www.dreamstime.com/photos-images/kanye-west.html'
+]
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  response = HTTP.get('https://api.kanye.rest/')
+ 
+  @api_data = JSON.parse(response.body)
+
+  page = Nokogiri::HTML(open('https://www.dreamstime.com/photos-images/kanye-west.html'))
+  
+  image_urls = page.css('img').map { |img| img['src'] }
+  
+  random_image_url = image_urls.sample
+  
+  image_data = open(random_image_url).read
+  
+  content_type 'image/jpeg' 
+  
+  image_data
+
+  erb(:main)
 end
