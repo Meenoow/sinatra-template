46711a6eafa075e0bfcdbe1ffe30cffb7c4442f2
Meenoow <100647073+Meenoow@users.noreply.github.com>
2023-09-28 19:09:41 +0000

    Bootstrap not currently working

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby "3.2.1"
 gem "sinatra"
 gem "sinatra-contrib"
 
+gem 'bootstrap', '~> 5.2.3'
+
 # Use Puma as the app server
 gem "puma", "~> 5.0"
 
--- app.rb
+++ app.rb
@@ -4,25 +4,25 @@ require "sinatra/activerecord"
 require "http"
 require 'open-uri'
 
-IMAGE_URLS = [
-  'https://variety.com/wp-content/uploads/2022/10/Kanye-WEst-1.jpg?w=1000.jpg',
-  'https://ca-times.brightspotcdn.com/dims4/default/bdf7dde/2147483647/strip/true/crop/3141x2343+0+0/resize/1200x895!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fc1%2F84%2F220dd37b2186542b6ed6f8433f25%2F757ab655cb214572ab883e7b0e518ccc.jpg',
-  'https://ichef.bbci.co.uk/news/976/cpsprodpb/14927/production/_127336248_314248636f734c3e432a4639fd341db087ff3ba2.jpg',
+#IMAGE_URLS = [
+  #'https://variety.com/wp-content/uploads/2022/10/Kanye-WEst-1.jpg?w=1000.jpg',
+  #'https://ca-times.brightspotcdn.com/dims4/default/bdf7dde/2147483647/strip/true/crop/3141x2343+0+0/resize/1200x895!/quality/75/?#url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.#com%2Fc1%2F84%2F220dd37b2186542b6ed6f8433f25%2F757ab655cb214572ab883e7b0e518ccc.jpg',
+  #'https://ichef.bbci.co.uk/news/976/cpsprodpb/14927/production/_127336248_314248636f734c3e432a4639fd341db087ff3ba2.jpg',
   
-]
+#]
 
 get("/") do
   response = HTTP.get('https://api.kanye.rest/')
  
   @api_data = JSON.parse(response.body)
 
-  random_image_url = IMAGE_URLS.sample
+  #random_image_url = IMAGE_URLS.sample
 
-  image_data = open(random_image_url).read
+  #image_data = open(random_image_url).read
 
-  content_type 'image/jpeg'
+  #content_type 'image/jpeg'
   
-  image_data
+  #image_data
 
   erb(:main)
 end
